3fd57b3eb8acec92028992c44bd23fb011f6934f
manojLovesCoding <mkum9417@gmail.com>
2024-03-09 15:56:29 +0530

    I removed preArrow Componenet

--- src/components/HeroCarousel/Arrows.component.js
+++ src/components/HeroCarousel/Arrows.component.js
@@ -11,15 +11,4 @@ export const NextArrow = (props) => {
     )
 };
 
-export const PrevArrow = (props) => {
-    return (
-        <>
-            <div className={props.className}
-                style={{
-                    ...props.style, backgroundColor: "black"
-                }}
-                onClick={props.onClick} />
-        </>
-    )
-};
 
