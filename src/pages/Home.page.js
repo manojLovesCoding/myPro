53a8241c315a85de6a805b876d196d3abecb5881
manojLovesCoding <mkum9417@gmail.com>
2024-03-11 13:21:49 +0530

    modified premierComponent file and HomePage file

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -3,19 +3,22 @@ import EntertainmentCardSlider from "../components/Entertainment/Entertainment.c
 import { Premier } from "../components/Premier/Premier.component";
 const HomePage = () => {
     return (<>
-        <div className="container mx-auto px-4">
-            <h1 className="text-2xl font-bold text-gray-800">The Best of Entertainment</h1>
-            <EntertainmentCardSlider />
-        </div>
-
-        <div className="bg-navCol-300 py-16">
-            <div className="flex">
-                <img src="https://assets-in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-banner-web-collection-202208191200.png" alt="Rupay" className="w-full h-full" />
-            </div>
+        <div className="flex flex-col gap-10">
             <div className="container mx-auto px-4">
-                <Premier />
+                <h1 className="text-2xl font-bold text-gray-800">The Best of Entertainment</h1>
+                <EntertainmentCardSlider />
             </div>
 
+            <div className="bg-navCol-300 py-16">
+
+                <div className="container mx-auto px-4 ">
+                    <div className="flex">
+                        <img src="https://assets-in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-banner-web-collection-202208191200.png" alt="Rupay" className="w-full h-full" />
+                    </div>
+                    <Premier />
+                </div>
+
+            </div>
         </div>
 
     </>
