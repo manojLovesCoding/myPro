1e9fd9320456d9b3d882db2b53bddc85c639acf1
manojLovesCoding <mkum9417@gmail.com>
2024-03-13 18:40:44 +0530

    Movie.page.js modified by creating MovieHero.component.js

---
+++ src/components/MovieHero/MovieHero.component.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MovieHero = () => {
+    return (
+        <>
+            <div className="md:hidden"  >
+                <img src="https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/shaitaan-et00384234-1706179766.jpg" alt="poster" />
+            </div>
+            <div className="hidden md-block lg:hidden"></div>
+            <div className="hidden lg:block"></div>
+        </>
+    );
+};
+
+export default MovieHero;
--- src/pages/Movie.page.js
+++ src/pages/Movie.page.js
@@ -1,11 +1,13 @@
 import React from "react";
+import MovieHero from "../components/MovieHero/MovieHero.component";
+/*
+<div><img src="https://getwallpapers.com/wallpaper/full/b/4/a/1267876-download-movie-poster-wallpaper-1920x1080.jpg" alt="" />
+/div>*/
 
 const Movie = () => {
     return (
         <>
-            <div>
-                <img src="https://getwallpapers.com/wallpaper/full/b/4/a/1267876-download-movie-poster-wallpaper-1920x1080.jpg" alt="" />
-            </div>
+            <MovieHero />
         </>
     );
 };
