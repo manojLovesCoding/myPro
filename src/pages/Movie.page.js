781461ee8f917e6d1fe5b08ae32f8f86c104861d
manojLovesCoding <mkum9417@gmail.com>
2024-03-14 17:07:21 +0530

    Modified Movie.Page.js by adding h1 and p elements for the content About_the_movie

--- src/pages/Movie.page.js
+++ src/pages/Movie.page.js
@@ -8,6 +8,16 @@ const Movie = () => {
     return (
         <>
             <MovieHero />
+            <div className="my-12 container mx-auto px-4 lg:w-1/2 lg:ml-20">
+                <div className="flex flex-col items-start gap-3">
+                    <h2 className="text-gray-800 font-bold text-2xl">About the movie</h2>
+                    <p>Kabir and his family`s fun weekend retreat takes a nightmarish turn when they let a friendly but mysterious stranger into their house. As the clock ticks, the family will be forced to confront their worst fears in this gripping, edge-of-the-seat supernatural-thriller that deals with the sinister elements of Indian Black Magic.</p>
+                </div>
+                <div className="my-8">
+                    <hr />
+                </div>
+
+            </div>
         </>
     );
 };
