80dc144cb446945c30b73581f0fb44f56acebce7
manojLovesCoding <mkum9417@gmail.com>
2024-03-14 17:40:58 +0530

    created Applicable offers by modifying in the Movie.page.js

--- src/pages/Movie.page.js
+++ src/pages/Movie.page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BiMoviePlay } from "react-icons/bi";
 import MovieHero from "../components/MovieHero/MovieHero.component";
 /*
 <div><img src="https://getwallpapers.com/wallpaper/full/b/4/a/1267876-download-movie-poster-wallpaper-1920x1080.jpg" alt="" />
@@ -15,6 +16,21 @@ const Movie = () => {
                 </div>
                 <div className="my-8">
                     <hr />
+                </div>
+                <div className="flex flex-col items-start gap-3">
+                    <h1 className="text-gray-800 font-bold text-2xl">Applicable Offers</h1>
+                    <div className="flex items-start gap-2 bg-yellow-100 border-yellow-400 border-dashed border-2 rounded-md p-3 w-96">
+                        <div className="w-8 h-8">
+                            <BiMoviePlay className="w-full h-full" />
+                        </div>
+
+                        <div className="flex flex-col items-start">
+                            <h3 className="text-gray-900 text-lg">Filmy Pass</h3>
+                            <p className="test-gray-600 text-sm">
+                                Get Rs.75* off on 3 movies you buy/rent on Strem. Buy Filmy Pass @Rs.99
+                            </p>
+                        </div>
+                    </div>
                 </div>
 
             </div>
