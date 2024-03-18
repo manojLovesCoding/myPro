2b29743d60b26351d3e1a848f1ff39e8be27da0c
manojLovesCoding <mkum9417@gmail.com>
2024-03-18 09:20:10 +0530

    modified the image src and Dynamically rendered the image src in HerCarousel.component,js

--- src/components/HeroCarousel/HerCarousel.component.js
+++ src/components/HeroCarousel/HerCarousel.component.js
@@ -16,7 +16,7 @@ const HeroCarousal = () => {
     useEffect(() => {
         const requestNowPlayingMovies = async () => {
             const getImages = await axios.get("/movie/now_playing");
-            console.log(getImages);
+            setImages(getImages.data.results);
         };
         requestNowPlayingMovies();
     }, []);
@@ -61,7 +61,7 @@ const HeroCarousal = () => {
                     {
                         images.map((image) => (
                             <div className="w-full h-64 md:h-80 py-3">
-                                <img src={image} alt="testing" className="w-full h-full rounded-md" />
+                                <img src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`} alt="testing" className="w-full h-full rounded-md" />
                             </div>
                         ))
                     }
@@ -73,7 +73,7 @@ const HeroCarousal = () => {
                     {
                         images.map((image) => (
                             <div className="w-full h-96 px-2 py-3">
-                                <img src={image} alt="testing" className="w-full h-full rounded-md" />
+                                <img src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`} alt="testing" className="w-full h-full rounded-md" />
                             </div>
                         ))
                     }
