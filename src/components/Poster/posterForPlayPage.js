e5d6a05d666d983bee03f8eb966b8df48639abb6
manojLovesCoding <mkum9417@gmail.com>
2024-03-19 11:54:32 +0530

    created a new component named posterForPlayPage.js for play.page.js

--- src/components/Poster/posterForPlayPage.js
+++ src/components/Poster/posterForPlayPage.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-//props => src,title,subtitle,isDark(bool)
-
-const Poster = (props) => {
+const PosterForPlayPage = (props) => {
     return (
         <>
             <div className="flex flex-col items-start gap-2 px-3">
                 <div className="h-80">
-                    <img src={`https://image.tmdb.org/t/p/original${props.poster_path}`} alt={props.original_title} className="w-full h-full rounded-xl" />
+                    <img src={props.src} alt={props.original_title} className="w-full h-full rounded-xl" />
                 </div>
                 <h3 className={`text-lg font-bold ${props.isDark ? "text-white" : "text-gray-700"
                     }`}>{props.title}</h3>
@@ -18,5 +16,4 @@ const Poster = (props) => {
     );
 };
 
-
-export default Poster;
+export default PosterForPlayPage;
--- src/pages/Play.page.js
+++ src/pages/Play.page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Poster from "../components/Poster/poster.component";
+import PosterForPlayPage from "../components/Poster/posterForPlayPage";
 import PlaysFilter from "../components/PlaysFilters/PlaysFilter.component";
 
 const Plays = () => {
@@ -12,29 +12,29 @@ const Plays = () => {
 
                         <div className="flex flex-wrap">
                             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                                <Poster src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
+                                <PosterForPlayPage src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC4zLzEwICAxMy43SyBWb3Rlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00318073-vnxhzuzaak-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 399 onwards " />
                             </div>
                             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                                <Poster src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
+                                <PosterForPlayPage src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
                             </div>
                             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                                <Poster src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
+                                <PosterForPlayPage src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
                             </div>
                             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                                <Poster src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
+                                <PosterForPlayPage src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
                             </div>
 
                             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                                <Poster src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
+                                <PosterForPlayPage src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
                             </div>
                             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                                <Poster src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
+                                <PosterForPlayPage src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
                             </div>
                             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                                <Poster src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
+                                <PosterForPlayPage src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
                             </div>
                             <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
-                                <Poster src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
+                                <PosterForPlayPage src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-text,ie-U2F0LCAyMCBBcHI%3D,fs-29,co-FFFFFF,ly-612,lx-24,pa-8_0_0_0,l-end/et00349259-xgeljpqevj-portrait.jpg" title="Mohabbat Zindabaad by Jai Singh" subtitle="English/Hindi/Urdu ₹ 499 onwards " />
                             </div>
                         </div>
                     </div>
